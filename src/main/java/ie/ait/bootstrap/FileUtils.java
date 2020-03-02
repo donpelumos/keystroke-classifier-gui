3e2db2dda983243a6b58b2e2a2e7a6c9d4a6b798
Pelumi Oyefeso <donpelumos@gmail.com>
2020-03-02 07:19:39 +0000

    create mock file on bootstrap.

--- src/main/java/ie/ait/bootstrap/FileUtils.java
+++ src/main/java/ie/ait/bootstrap/FileUtils.java
@@ -1,8 +1,13 @@
 package ie.ait.bootstrap;
 
+import com.opencsv.CSVWriter;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Pelumi.Oyefeso on 02-Mar-2020
@@ -12,19 +17,39 @@ public class FileUtils {
         Path path = Paths.get(System.getProperty("user.dir")+ File.separator+"keystroke-classifier-shared-folder");
         boolean pathExists = Files.exists(path, new LinkOption[]{ LinkOption.NOFOLLOW_LINKS});
         if(pathExists){
-            //dd
-            System.out.println("");
+            try{
+                writeEmpty(path);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
         }
         else{
             try {
                 path = Files.createDirectory(path);
             } catch(FileAlreadyExistsException e){
                 // the directory already exists.
+                e.printStackTrace();
             } catch (IOException e) {
                 //something else went wrong
                 e.printStackTrace();
             }
         }
+    }
+
+    public void writeEmpty(Path path) throws Exception {
+        List<String[]> sample = new ArrayList<>();
+        sample.add(new String[]{"Name","Country"});
+        sample.add(new String[]{"John","Canadian"});
+        csvWriter(sample, Paths.get(path.toString()+File.separator+"sample.csv"));
+    }
+
+    private void csvWriter(List<String[]> stringArray, Path path) throws Exception {
+        CSVWriter writer = new CSVWriter(new FileWriter(path.toString()));
+        for (String[] array : stringArray) {
+            writer.writeNext(array);
+        }
+        writer.close();
     }
 
 }
