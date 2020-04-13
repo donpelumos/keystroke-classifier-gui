105106e989ab92735a712330b0e220ecd032035e
Pelumi <donpelumos@gmail.com>
2020-04-13 08:09:04 +0100

    - added reset button to 'test' view (scene).

--- src/main/java/ie/ait/controllers/TestController.java
+++ src/main/java/ie/ait/controllers/TestController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 
+import java.util.ArrayList;
+
 /**
  * Created by Pelumi.Oyefeso on 24-Mar-2020
  */
@@ -22,6 +24,8 @@ public class TestController {
     @FXML
     private Button doneButton;
     @FXML
+    private Button resetButton;
+    @FXML
     private Label resultLabel;
     @FXML
     private Label classificationTechniqueLabel;
@@ -33,5 +37,38 @@ public class TestController {
         sourceTextArea.setEditable(false);
         classificationTechniqueComboBox.setVisible(false);
         classificationTechniqueLabel.setVisible(false);
+        initializeValues();
+        /*
+        fetchTextToType();
+        initializeMaps();
+        handleEvents();
+        */
+    }
+
+    private void initializeValues(){
+        /*
+        isTextCompleted = false;
+        resetButton.setDisable(true);
+        newUserRadioButton.setDisable(true);
+        existingUserRadioButton.setSelected(false);
+        existingUserRadioButton.setDisable(false);
+        textArea.setWrapText(true);
+        pressedKeysList = new ArrayList<>();
+        releasedKeysList = new ArrayList<>();
+        existingUserHBox.setVisible(false);
+        newUserHBox.setVisible(true);
+        continueButton.setDisable(true);
+        existingUserLabel.setVisible(false);
+        textArea.setEditable(false);
+        textArea.setDisable(true);
+        sourceTextArea.setDisable(false);
+        sourceTextArea.setEditable(false);
+        getTrainedUsers();
+        if(existingUsersComboBox.getItems().size() == 0) {
+            for (String user : trainedUsers) {
+                existingUsersComboBox.getItems().add(user);
+            }
+        }
+        */
     }
 }
