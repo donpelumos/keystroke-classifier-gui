2543ef0b518cd76848984eadeaa7bf46f74bf892
Pelumi <donpelumos@gmail.com>
2020-04-13 07:58:41 +0100

    - hide classification selection option thus defaukting to only KNN for classification.

--- src/main/java/ie/ait/controllers/TestController.java
+++ src/main/java/ie/ait/controllers/TestController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 
 /**
  * Created by Pelumi.Oyefeso on 24-Mar-2020
@@ -17,14 +18,20 @@ public class TestController {
     @FXML
     private ComboBox<String> classificationTechniqueComboBox;
     @FXML
+    private HBox classificationTechniqueHBox;
+    @FXML
     private Button doneButton;
     @FXML
     private Label resultLabel;
+    @FXML
+    private Label classificationTechniqueLabel;
     /**
      The "initialize" method is automatically called because this class is annotated with the @FXML.
      */
     public void initialize(){
         classificationTechniqueComboBox.getItems().addAll("KNN","SVM","DECISION TREES");
         sourceTextArea.setEditable(false);
+        classificationTechniqueComboBox.setVisible(false);
+        classificationTechniqueLabel.setVisible(false);
     }
 }
